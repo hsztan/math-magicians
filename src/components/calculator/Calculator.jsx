973093fb7b03c742193cd9bb8dd88339d43b9238
Henry Nawrocki <hdev@sztanski.com>
2022-04-19 14:49:43 -0500

    Changed functional components to class-based components

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,78 +1,85 @@
+import React from 'react';
 import './calculator.scss';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="calculator__display">0</div>
-    <div className="calculator__keypad">
-      <div className="calculator__keypad-row">
-        <button type="button" className="calculator__keypad-button">
-          AC
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          +/-
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          %
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          &divide;
-        </button>
+export default class Calculator extends React.PureComponent {
+  render() {
+    return (
+      <div className="calculator">
+        <div className="calculator__display">0</div>
+        <div className="calculator__keypad">
+          <div className="calculator__keypad-row">
+            <button type="button" className="calculator__keypad-button">
+              AC
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              +/-
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              %
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              &divide;
+            </button>
+          </div>
+          <div className="calculator__keypad-row">
+            <button type="button" className="calculator__keypad-button">
+              7
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              8
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              9
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              &times;
+            </button>
+          </div>
+          <div className="calculator__keypad-row">
+            <button type="button" className="calculator__keypad-button">
+              4
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              5
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              6
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              -
+            </button>
+          </div>
+          <div className="calculator__keypad-row">
+            <button type="button" className="calculator__keypad-button">
+              1
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              2
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              3
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              +
+            </button>
+          </div>
+          <div className="calculator__keypad-row">
+            <button
+              id="zero"
+              type="button"
+              className="calculator__keypad-button"
+            >
+              0
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              .
+            </button>
+            <button type="button" className="calculator__keypad-button">
+              =
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="calculator__keypad-row">
-        <button type="button" className="calculator__keypad-button">
-          7
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          8
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          9
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          &times;
-        </button>
-      </div>
-      <div className="calculator__keypad-row">
-        <button type="button" className="calculator__keypad-button">
-          4
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          5
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          6
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          -
-        </button>
-      </div>
-      <div className="calculator__keypad-row">
-        <button type="button" className="calculator__keypad-button">
-          1
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          2
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          3
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          +
-        </button>
-      </div>
-      <div className="calculator__keypad-row">
-        <button id="zero" type="button" className="calculator__keypad-button">
-          0
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          .
-        </button>
-        <button type="button" className="calculator__keypad-button">
-          =
-        </button>
-      </div>
-    </div>
-  </div>
-);
-
-export default Calculator;
+    );
+  }
+}
