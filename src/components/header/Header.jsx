6d9018abbcb154225c8168d808a83faf4c2a43b0
Henry Nawrocki <hdev@sztanski.com>
2022-04-26 10:29:58 -0500

    Style header and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
-    <Link to="/">Math Magicians</Link>
+    <Link id="logo" to="/">
+      Math Magicians
+    </Link>
     <ul className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/calculator">Calculator</Link>
-      <Link to="/quote">Quote</Link>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quote</Link>
+      </li>
     </ul>
   </div>
 );
