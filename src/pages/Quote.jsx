2ddfb20d5efae64c2d835c6f3a156f462e179dd9
Henry Nawrocki <hdev@sztanski.com>
2022-04-26 11:02:39 -0500

    Change p element size

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -2,7 +2,7 @@ const Quote = () => (
   <div className="main-content quote">
     <h1>
       Mathematics is not about numbers, equations, computations, or algorithms:
-      it is about understanding. --William Paul Thurston
+      it is about understanding. &mdash;William Paul Thurston
     </h1>
   </div>
 );
